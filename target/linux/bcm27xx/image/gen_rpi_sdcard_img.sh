64894893e7b9dee2bc3e593258c55b55bd4ea384
Hydra WRT builder <nwf@cs.jhu.edu>
2016-11-07 16:58:44 +0000

    Speed up gen_rpi_sdcard_img.sh

--- target/linux/bcm27xx/image/gen_rpi_sdcard_img.sh
+++ target/linux/bcm27xx/image/gen_rpi_sdcard_img.sh
@@ -17,13 +17,13 @@ sect=63
 
 set $(ptgen -o $OUTPUT -h $head -s $sect -l 4096 -t c -p ${BOOTFSSIZE}M -t 83 -p ${ROOTFSSIZE}M)
 
-BOOTOFFSET="$(($1 / 512))"
+BOOTOFFSET="$1"
 BOOTSIZE="$(($2 / 512))"
-ROOTFSOFFSET="$(($3 / 512))"
+ROOTFSOFFSET="$3"
 ROOTFSSIZE="$(($4 / 512))"
 
-dd bs=512 if="$BOOTFS" of="$OUTPUT" seek="$BOOTOFFSET" conv=notrunc
-dd bs=512 if="$ROOTFS" of="$OUTPUT" seek="$ROOTFSOFFSET" conv=notrunc
+dd bs=65536 if="$BOOTFS" of="$OUTPUT" seek="$BOOTOFFSET" conv=notrunc oflag=seek_bytes
+dd bs=65536 if="$ROOTFS" of="$OUTPUT" seek="$ROOTFSOFFSET" conv=notrunc oflag=seek_bytes
 
 
 
